9733fda456cdce84a213cb03c7ab80fac4f307da
jerold16 <jeroldraja12@gmail.com>
2024-01-06 17:43:14 +0530

    social

--- FrontEnd/src/Component/BlogDetails.jsx
+++ FrontEnd/src/Component/BlogDetails.jsx
@@ -103,10 +103,10 @@ const BlogDetails = () => {
                      poiints.map((data,index)=>{
                         return(
                 <p className='text-slate-600 w-1/2 flex gap-2'> 
-                 <svg className='mt-1' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                 <svg className='' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"/>
 </svg>
-                <p className='w-10/12'>{data.point} </p> </p>                          
+                <p className='w-10/12 '>{data.point} </p> </p>                          
                         )
                     })
                 }
